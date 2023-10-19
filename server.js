376542acdaebc3bdeda51e1daf246f813338c864
shlok0070 <shlok0070@gmail.com>
2023-10-19 09:53:50 +0530

    added login page, with validation.

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const User = require('./User');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const bcrypt = require('bcrypt');
 const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
 app.use(bodyParser.json());  // for parsing application/json
 app.use('/signup', express.static(path.join(__dirname, 'public')));
@@ -12,10 +15,12 @@ app.post('/signup', async (req, res) => {
     const { name, email, password } = req.body;
     
     try {
+        const hashedPassword = await bcrypt.hash(password, 10);  // 10 is the number of salt rounds
+
         const user = await User.create({
             name,
             email,
-            password,
+            password: hashedPassword,
             createdAt: new Date(),
             updatedAt: new Date()
         });
@@ -27,6 +32,33 @@ app.post('/signup', async (req, res) => {
         } else {
             res.status(500).json({ message: 'An error occurred' });  // Respond with JSON
         }
+    }
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    
+    try {
+        const user = await User.findOne({ where: { email } });
+        
+        if (!user) {
+            return res.status(400).json({ message: 'Email does not exist' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Wrong password' });
+        }
+        
+        res.status(200).json({ message: 'User logged in successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred' });
     }
 });
 
